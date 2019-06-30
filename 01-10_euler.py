376b624720ddcb526edb33dab04042180c65a493
Brian Roepke <broepke@gmail.com>
2019-06-30 07:31:30 -0700

    Question 3

--- 01-10_euler.py
+++ 01-10_euler.py
@@ -32,3 +32,19 @@ while fib < 4000000:
     second = fib
 
 print('Question 2 =', total)
+
+# The prime factors of 13195 are 5, 7, 13 and 29.
+# What is the largest prime factor of the number 600851475143
+
+
+def largest_prime_factor(n):
+    i = 2
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+    return n
+
+
+print('Question 3 =', largest_prime_factor(600851475143))
