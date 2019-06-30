0f80259f7956830e7a04f11d965ca0b8c7687e50
Brian Roepke <broepke@gmail.com>
2019-06-30 07:33:44 -0700

    Update to Q3

--- 01-10_euler.py
+++ 01-10_euler.py
@@ -37,14 +37,12 @@ print('Question 2 =', total)
 # What is the largest prime factor of the number 600851475143
 
 
-def largest_prime_factor(n):
-    i = 2
-    while i * i <= n:
-        if n % i:
-            i += 1
-        else:
-            n //= i
-    return n
+n = 600851475143
+i = 2
 
+while i * i < n:
+    while n % i == 0:
+        n = n / i
+    i = i + 1
 
-print('Question 3 =', largest_prime_factor(600851475143))
+print('Question 3 =', int(n))
