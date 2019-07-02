b90357644d6f09006d52fe1ef60522e64b7d6e82
Brian Roepke <broepke@gmail.com>
2019-07-01 18:22:16 -0700

    Update 01-10_euler.py

--- 01-10_euler.py
+++ 01-10_euler.py
@@ -1,8 +1,11 @@
 # solving the Euler Problems
 import numpy as np
 from math import sqrt
+import time
 
-#'''
+start_time = time.time()
+
+'''
 
 # Question 1
 # If we list all the natural numbers below 10 that are multiples of 3 or 5,
@@ -51,6 +54,7 @@ while i * i < n:
 
 print('Question 3 =', int(n))  # 6857
 
+
 # A palindromic number reads the same both ways. The largest palindrome
 # made from the product of two 2-digit numbers is 9009 = 91 × 99.
 # Find the largest palindrome made from the product of two 3-digit numbers.
@@ -83,7 +87,6 @@ for a in range(999, 900, -1):
 
 print('Question 4 =', largest_found,
       '(', largest_a, largest_b, ')')  # 906609 ( 993 913 )
-
 
 # 2520 is the smallest number that can be divided by each of the
 # numbers from 1 to 10 without any remainder.
@@ -130,7 +133,6 @@ b = b ** 2
 
 print('Question 6 =', b - a)  # 25164150
 
-
 # By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13,
 # we can see that the 6th prime is 13.
 # What is the 10 001st prime number?
@@ -151,3 +153,41 @@ while len(prime_list) < 10001:
     num += 1
 
 print('Question 7 =', prime_list[-1:])  # 104743
+
+
+# The four adjacent digits in the 1000-digit number that have the
+# greatest product are 9 × 9 × 8 × 9 = 5832.
+# Find the thirteen adjacent digits in the 1000-digit number that
+# have the greatest product. What is the value of this product?
+
+digits = '7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450'
+
+start = 0
+end = 13
+largest = 0
+
+for i in range(len(digits) + 1):
+    dig_str = digits[start:end]
+    total = 1
+    for dig in dig_str:
+        total *= int(dig)
+    start += 1
+    end += 1
+    if total > largest:
+        largest = total
+
+print('Question 8 =', largest)  # 23514624000
+
+'''
+
+# A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+#
+# a2 + b2 = c2
+# For example, 3^22 + 4^2 = 9 + 16 = 25 = 5^2.
+#
+# There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+# Find the product abc.
+
+print("Program took %s seconds to run." % (time.time() - start_time))
+
+print(sqrt())
