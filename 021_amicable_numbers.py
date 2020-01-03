96ac970aca42be39daa480a2f9cd098c36b541d9
Brian Roepke <broepke@gmail.com>
2020-01-03 13:06:25 -0800

    21 snapshot

---
+++ 021_amicable_numbers.py
@@ -0,0 +1,29 @@
+import time
+
+start_time = time.time()
+
+# Let d(n) be defined as the sum of proper divisors of n 
+# (numbers less than n which divide evenly into n).
+# If d(a) = b and d(b) = a, where a ≠ b, then a and b are an 
+# amicable pair and each of a and b are called amicable numbers.
+
+# For example, the proper divisors of 220 are
+# 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110; therefore d(220) = 284. 
+# The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
+
+# Evaluate the sum of all the amicable numbers under 10000.
+
+a = []
+target = 100
+
+for y in range(1,target):
+	for x in range(1,y):
+		if target % x == 0:
+			b = []
+			b.append(x)
+		a.append(sum(b))
+		
+
+print('Problem 21 =', a) # 
+
+print("Program took %s seconds to run." % (time.time() - start_time))
