cb128ee0fc0335cd9bef0de6baca1e6cda40950e
broepke <broepke@gmail.com>
2020-01-03 08:56:44 -0800

    Starting 19

---
+++ 019_counting_sundays.py
@@ -0,0 +1,23 @@
+import time
+
+start_time = time.time()
+
+# You are given the following information, but you may prefer to do some research for yourself.
+
+# 1 Jan 1900 was a Monday.
+# Thirty days has September,
+# April, June and November.
+# All the rest have thirty-one,
+# Saving February alone,
+# Which has twenty-eight, rain or shine.
+# And on leap years, twenty-nine.
+# A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
+# How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+
+
+
+
+
+print('Problem 19 =', ) # 21,124 total letters
+
+print("Program took %s seconds to run." % (time.time() - start_time))
