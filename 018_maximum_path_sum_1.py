423cd5f9c0a4df7ce5c819b55717cb4942c4bde4
broepke <broepke@gmail.com>
2020-01-02 18:20:42 -0800

    Update 018_maximum_path_sum_1.py
    Finished w/ Correct Answer

--- 018_maximum_path_sum_1.py
+++ 018_maximum_path_sum_1.py
@@ -58,53 +58,53 @@ tri = np.array([[75, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00],
                 [4, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 4, 23]])
 
 total = 75
-row = 1
+row = 0
 col = 0
 
+
 def get_largest_path(row, col, total):
-	
-	print(tri[row][col], total)
-	
-	# Find the biggest path by comparing each one and finding the max in an array
-	o1 = tri[row][col] + tri[row+1][col]
-	o2 = tri[row][col] + tri[row+1][col+1]
+    # total += tri[row][col]
 
-	o3 = tri[row][col+1] + tri[row+1][col+1]
-	o4 = tri[row][col+1] + tri[row+1][col+2]
-	
+    row += 1
 
-	# Increment the rows based on what the largest option was 
-	options = np.array([o1, o2, o3, o4])
-	print(options)
-	opt = np.where(options == np.amax(options))
-	
-	if opt[0][0] == 0:
-		print(tri[row][col])
-		total += tri[row][col]
-		row = row + 1
-		col = col
-	elif opt[0][0] == 1:
-		col = col + 1
-		print(tri[row][col])
-		total += tri[row][col]
-		row = row + 1
-	elif opt[0][0] == 2:
-		print(tri[row][col+1])
-		total += tri[row][col+1]
-		row = row + 1
-		col = col + 1
-	else:
-		print(tri[row][col+1])
-		total += tri[row][col+1]
-		row = row + 1
-		col = col + 2
+    # Find the biggest path by comparing each one and finding the max in an array
+    o1 = tri[row][col] + tri[row + 1][col]
+    o2 = tri[row][col] + tri[row + 1][col + 1]
 
-	return row, col, total
+    o3 = tri[row][col + 1] + tri[row + 1][col + 1]
+    o4 = tri[row][col + 1] + tri[row + 1][col + 2]
+
+    # Increment the rows based on what the largest option was
+    options = np.array([o1, o2, o3, o4])
+    opt = np.where(options == np.amax(options))
+
+    if opt[0][0] == 0:
+        total += tri[row][col]
+        total += tri[row + 1][col]
+        row += 1
+        col = col
+    elif opt[0][0] == 1:
+        total += tri[row][col]
+        total += tri[row + 1][col + 1]
+        row += 1
+        col = col + 1
+    elif opt[0][0] == 2:
+        total += tri[row][col + 1]
+        total += tri[row + 1][col + 1]
+        row += 1
+        col = col + 1
+    else:
+        total += tri[row][col + 1]
+        total += tri[row + 1][col + 2]
+        row += 1
+        col = col + 2
+
+    return row, col, total
 
 
 while row <= 13:
-	row, col, total = get_largest_path(row, col, total)
+    row, col, total = get_largest_path(row, col, total)
 
-print('Problem 18 =', total)  #
+print('Problem 18 =', total)  # 1074
 
 print("Program took %s seconds to run." % (time.time() - start_time))
