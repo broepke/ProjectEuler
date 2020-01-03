6ba4784ca44eb11e292ea8ec3f36fa6aa4c5a307
broepke <broepke@gmail.com>
2020-01-03 09:14:42 -0800

    Updating 67

--- 067_maximum_path_sum_2.py
+++ 067_maximum_path_sum_2.py
@@ -1,85 +1,37 @@
 import time
-import numpy as np
 
 start_time = time.time()
 
-# By starting at the top of the triangle below and moving to adjacent numbers on the row below,
-# the maximum total from top to bottom is 23.
-#
-# 3
-# 7 4
-# 2 4 6
-# 8 5 9 3
-#
-# That is, 3 + 7 + 4 + 9 = 23.
-#
-# Find the maximum total from top to bottom in triangle.txt (right click and 'Save Link/Target As...'),
-# a 15K text file containing a triangle with one-hundred rows.
-#
-# NOTE: This is a much more difficult version of Problem 18. It is not possible
-# to try every route to solve this problem, as there are 299 altogether!
-# If you could check one trillion (1012) routes every second it would take over twenty billion years
-# to check them all. There is an efficient algorithm to solve it. ;o)
+def listPrep(file):
+    f = open(file, 'r')
+    x = f.readlines()
+    f.close()
+
+    for i in range(0, len(x)):
+        for char in '\n':
+            x[i] = x[i].replace(char, '')
+        x[i] = x[i].split()
+
+    for i in range(0, len(x)):
+        for j in range(0, len(x[i])):
+            x[i][j] = int(x[i][j])
+
+    return x
 
 
-tri = np.genfromtxt('067_triangle.txt', delimiter="\t")
+binTree = listPrep('067_triangle.txt')
+
+for i in range(1, len(binTree)):
+    binTree[i][0] += binTree[i - 1][0]
+    binTree[i][len(binTree[i]) - 1] += binTree[i - 1][len(binTree[i - 1]) - 1]
 
 
-total = 59
-row = 0
-col = 0
+for i in range(2, len(binTree)):
+    for j in range(1, len(binTree[i]) - 1):
+        binTree[i][j] += max(binTree[i - 1][j - 1], binTree[i - 1][j])
 
+total = max(binTree[len(binTree) - 1])
 
-def get_largest_path(row, col, total):
-
-    print(total)
-
-    row += 1
-
-    # Find the biggest path by comparing each one and finding the max in an array
-    print(row, col)
-    o1 = tri[row][col] + tri[row + 1][col]
-    o2 = tri[row][col] + tri[row + 1][col + 1]
-
-    o3 = tri[row][col + 1] + tri[row + 1][col + 1]
-    o4 = tri[row][col + 1] + tri[row + 1][col + 2]
-
-    # Increment the rows based on what the largest option was
-    options = np.array([o1, o2, o3, o4])
-    opt = np.where(options == np.amax(options))
-
-    if opt[0][0] == 0:
-        total += tri[row][col]
-        total += tri[row + 1][col]
-        row += 1
-        col = col
-    elif opt[0][0] == 1:
-        total += tri[row][col]
-        total += tri[row + 1][col + 1]
-        row += 1
-        col = col + 1
-    elif opt[0][0] == 2:
-        total += tri[row][col + 1]
-        total += tri[row + 1][col + 1]
-        row += 1
-        col = col + 1
-    else:
-        total += tri[row][col + 1]
-        total += tri[row + 1][col + 2]
-        row += 1
-        col = col + 2
-
-    # print(row, col, total)
-
-    return row, col, total
-
-
-while row <= 97:
-    row, col, total = get_largest_path(row, col, total)
-
-
-print('Problem 18 =', total)  # 1074
+print('Problem 18 =', total)  # 7273
 
 print("Program took %s seconds to run." % (time.time() - start_time))
-
-
