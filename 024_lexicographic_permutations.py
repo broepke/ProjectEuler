15141f9bedf544a21e540f3aa3bcee0d5c0abe4d
Brian Roepke <broepke@users.noreply.github.com>
2020-02-29 20:24:13 -0800

    Problem 24 Complete

--- 024_lexicographic_permutations.py
+++ 024_lexicographic_permutations.py
@@ -1,4 +1,5 @@
 import time
+import itertools
 start_time = time.time()
 
 # A permutation is an ordered arrangement of objects. For example,
@@ -12,7 +13,12 @@ start_time = time.time()
 # What is the millionth lexicographic permutation of the
 # digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
 
+rgn = 10
+
+a = list(itertools.permutations(range(rgn)))
 
 
-print('Problem 24 =')
+b = (a[999999])
+
+print('Problem 24 =', b) #2783915460
 print("Program took %s seconds to run." % (time.time() - start_time))
