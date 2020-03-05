469b7736d80dab5894aa0a90faeecb80eb12f8b1
Brian Roepke <broepke@gmail.com>
2020-03-05 10:04:38 -0800

    Added 29 and 30

--- 028_number_spiral_diagonals.py
+++ 028_number_spiral_diagonals.py
@@ -1,7 +1,4 @@
 import time
-import euler
-import numpy as np
-import statistics
 start_time = time.time()
 
 # Starting with the number 1 and moving to the right in a clockwise direction a 5 by 5 spiral is formed as follows:
@@ -16,13 +13,27 @@ start_time = time.time()
 
 # What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed in the same way?
 
-size = (5,5)
-# size = (1001,1001)
-medium = 3 
-# medium = 501
-m = np.zeros(size)
+median = 501 #3 to test, 501 for solution  
+
+# initialize this outside the center.  The center is always 1 so it will be 
+# easier to caclulate the loops after adding and moving past the center.
+start = 3
+corners = [1]
 
 
+# since each of these are a perfect four sided square, you can increment up on the 
+# corners by odd values, and using the square of that to represent the range of the loop
+# then just figure out the corners based on that. 
+for c in range(median-1):
+	corner = start**2
+	corners.append(corner)
+	for n in range(3):
+		corner = corner-(start-1)
+		corners.append(corner)
+	start += 2
+	
+total = sum(corners)
 
-print('Problem 28 =') # 
+
+print('Problem 28 =', total) # 669171001
 print("Program took %s seconds to run." % (time.time() - start_time))
