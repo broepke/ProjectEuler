689c43fdaefeec6b66b45fdef8b5fb2debe6182c
Brian Roepke <broepke@gmail.com>
2020-03-05 10:51:50 -0800

    Completed 30
    And stub 31 added

--- 030_digit_fifth_powers.py
+++ 030_digit_fifth_powers.py
@@ -13,5 +13,19 @@ start_time = time.time()
 
 # Find the sum of all the numbers that can be written as the sum of fifth powers of their digits.
 
-print('Problem 30 =') #
+start = 4000
+end = 200000
+p_digits = []
+
+for dig in range(start,end):
+	total = 0
+	for num in str(dig):
+		total += int(num)**5
+	if total == dig:
+		p_digits.append(dig)
+		
+		
+print(p_digits)
+
+print('Problem 30 =', sum(p_digits)) #443839
 print("Program took %s seconds to run." % (time.time() - start_time))
