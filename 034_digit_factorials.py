105ba67d1b3806ce940c7c76747221c084c4a80b
broepke <broepke@gmail.com>
2020-07-17 07:26:30 -0700

    Create 034_digit_factorials.py

---
+++ 034_digit_factorials.py
@@ -0,0 +1,26 @@
+import time
+start_time = time.time()
+from math import factorial
+
+# 145 is a curious number, as 1! + 4! + 5! = 1 + 24 + 120 = 145.
+#
+# Find the sum of all numbers which are equal to the sum of the factorial of their digits.
+#
+# Note: as 1! = 1 and 2! = 2 are not sums they are not included.
+
+num_list = []
+
+for x in range(3,146):
+    num_total = 0
+
+    for i in str(x):
+        num_total += factorial(int(i))
+
+    if num_total == x:
+        num_list.append(x)
+
+print(num_list)
+
+
+print('Problem 34 =')
+print("Program took %s seconds to run." % (time.time() - start_time))
