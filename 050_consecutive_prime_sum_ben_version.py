8fedd29f6895cb199953582867f1041af70c3ba4
broepke <broepke@gmail.com>
2020-07-28 11:02:55 -0700

    Trying Ben's way

--- 050_consecutive_prime_sum_ben_version.py
+++ 050_consecutive_prime_sum_ben_version.py
@@ -70,3 +70,59 @@ prime_max = max(prime_sums)
 
 print('Problem 50 =', prime_max, "and was", longest, "digits in length.")
 print("Program took %s seconds to run." % (time.time() - start_time))
+'''
+//
+//  main.cpp
+//  ConsecutivePrimeSum
+//
+//  Created by Ben Cochran on 5/9/20.
+//  Copyright © 2020 Ben Cochran. All rights reserved.
+//
+//  https://projecteuler.net/problem=50
+​
+#include <iostream>
+#include "../primes.h"
+​
+using namespace std;
+​
+#define MILLION 1000000
+//#define MILLION 1000
+​
+int main(int argc, const char * argv[]) {
+
+    cout << "Consecutive prime sum - problem 50: https://projecteuler.net/problem=50\n";
+
+    Primes p ("../1MPrimes");
+
+    int answerPrime = 0;
+    int answerPrimeSumTermCount = 0;
+
+    for (int n=2; p.nthPrime (n) < MILLION; n++){
+        int sum = 0;
+        int termCount = 0;
+        for ( int j=1; j < n; j++){
+            sum += p.nthPrime (j);
+            termCount++;
+​
+            // If the sum is to large, take awasy from the bottom side until corrected.
+            for (int k=1; termCount > 0 && sum > p.nthPrime (n) && k < j; k++){
+                sum -= p.nthPrime(k);
+                termCount--;
+            }
+
+            if ( sum == p.nthPrime (n) )
+                break;
+        }
+
+        if ( termCount >  answerPrimeSumTermCount){
+            answerPrime = (int)p.nthPrime (n);
+            answerPrimeSumTermCount = termCount;
+        }
+    }
+
+    cout << "Prime, below one-million, can be written as the sum of the most consecutive primes is: "
+         << answerPrime << "(" << answerPrimeSumTermCount << ")" << endl;
+
+    return 0;
+}
+'''
