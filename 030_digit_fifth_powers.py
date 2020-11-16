4fb77d5712b57d516c8e36dd99322b4ba9f761a6
broepke <broepke@gmail.com>
2020-11-15 18:17:07 -0800

    Linter

--- 030_digit_fifth_powers.py
+++ 030_digit_fifth_powers.py
@@ -1,7 +1,8 @@
 import time
 start_time = time.time()
 
-# Surprisingly there are only three numbers that can be written as the sum of fourth powers of their digits:
+# Surprisingly there are only three numbers that can be written as
+# the sum of fourth powers of their digits:
 
 # 1634 = 1^4 + 6^4 + 3^4 + 4^4
 # 8208 = 8^4 + 2^4 + 0^4 + 8^4
@@ -11,21 +12,22 @@ start_time = time.time()
 
 # The sum of these numbers is 1634 + 8208 + 9474 = 19316.
 
-# Find the sum of all the numbers that can be written as the sum of fifth powers of their digits.
+# Find the sum of all the numbers that can be written as the sum of
+# fifth powers of their digits.
 
 start = 4000
 end = 200000
 p_digits = []
 
-for dig in range(start,end):
-	total = 0
-	for num in str(dig):
-		total += int(num)**5
-	if total == dig:
-		p_digits.append(dig)
-		
-		
+for dig in range(start, end):
+    total = 0
+    for num in str(dig):
+        total += int(num)**5
+    if total == dig:
+        p_digits.append(dig)
+
+
 print(p_digits)
 
-print('Problem 30 =', sum(p_digits)) #443839
+print('Problem 30 =', sum(p_digits))  # 443839
 print("Program took %s seconds to run." % (time.time() - start_time))
