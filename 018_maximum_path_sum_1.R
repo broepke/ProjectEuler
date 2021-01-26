9b5f949cd7a97c0f28776d428415da4c9d4db3de
Brian Roepke <broepke@gmail.com>
2021-01-25 16:30:40 -0800

    Updated with totally new solution

--- 018_maximum_path_sum_1.R
+++ 018_maximum_path_sum_1.R
@@ -49,62 +49,30 @@ t <- c(75, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00,
        63, 66, 04, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31, 00,
        04, 62, 98, 27, 23, 09, 70, 98, 73, 93, 38, 53, 60, 04, 23)
 
-tri <- matrix(t, nrow=15)
-tri <- apply(t(tri),2,rev)
+tri <- matrix(t, nrow = 15)
+tri <- apply(t(tri), 2, rev)
 tri <- flipud(tri)
-
-x = "75
-95 64
-17 47 82
-18 35 87 10
-20 04 82 47 65
-19 01 23 75 03 34
-88 02 77 73 07 63 67
-99 65 04 28 06 16 70 92
-41 41 26 56 83 40 80 70 33
-41 48 72 33 47 32 37 16 94 29
-53 71 44 65 25 43 91 52 97 51 14
-70 11 33 28 77 73 17 78 39 68 17 57
-91 71 52 38 17 14 91 43 58 50 27 29 48
-63 66 04 68 89 53 67 30 73 16 69 87 40 31
-04 62 98 27 23 09 70 98 73 93 38 53 60 04 23"
-
-#Split by line (\n) and then by space
-y <- str_split(x, "\n") %>% .[[1]] %>% str_split(" ")
-
-#Create 15 x 15 matrix of trieros to place values
-tri <- matrix(0, nrow = 15, ncol = 15)
-
-# Move the integer values into the Matrix
-
-for (row in rev(1:15)) {
-  for (col in 1:length(y[[row]])) {
-    tri[row, col] <- as.integer(y[[row]][col])
-    
-  }
-}
-
-z #Matrix
 
 
 # Matrix of zeros to store max values
-t <- matrix(0, nrow = 14, ncol = 14)
+new_tri <- matrix(0, nrow = 14, ncol = 14)
 
 for (row in rev(1:14)) {
   # Calculate Max values
-  for (col in 1:length(tri[row,][tri[row,] != 0])) {
-    t[row, col] <- max(tri[row, col] + tri[row + 1, col], tri[row, col] + tri[row + 1, col + 1])
-    
-    
+  for (col in 1:length(tri[row, ][tri[row, ] != 0])) {
+    new_tri[row, col] <-
+      max(tri[row, col] + tri[row + 1, col], tri[row, col] + tri[row + 1, col + 1])
   }
   
   # replace values in original Matrix with calculated max values
-  
-  for (col in 1:length(tri[row,][tri[row,] != 0])) {
-    tri[row, col] <- t[row, col]
+  for (col in 1:length(tri[row, ][tri[row, ] != 0])) {
+    tri[row, col] <- new_tri[row, col]
   }
 }
 
 tri[1:14, 1:14] #full pyramid of max values
 
-tri[1, 1] #top value
+print(paste("Problem 18 = ", tri[1, 1]))  # 1074
+
+end_time <- Sys.time()
+print(end_time - start_time)
