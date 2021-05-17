80a0bf41618c14bc1a2924a8a2d8665e0047556e
Brian Roepke <broepke@gmail.com>
2021-05-17 07:50:38 -0700

    Updates to 67

--- 067_maximum_path_sum.jl
+++ 067_maximum_path_sum.jl
@@ -1,57 +1,40 @@
+# By starting at the top of the triangle below and moving to adjacent numbers on
+# the row below, the maximum total from top to bottom is 23.
+#
+# 3 7 4 2 4 6 8 5 9 3
+#
+# That is, 3 + 7 + 4 + 9 = 23.
+#
+# Find the maximum total from top to bottom in triangle.txt (right click and
+# 'Save Link/Target As...'), a 15K text file containing a triangle with
+# one-hundred rows.
+#
+# NOTE: This is a much more difficult version of Problem 18. It is not possible
+# to try every route to solve this problem, as there are 299 altogether! If you
+# could check one trillion (1012) routes every second it would take over twenty
+# billion years to check them all. There is an efficient algorithm to solve it.
+# ;o)
+
 
 using DelimitedFiles
 
-# create the prime list from the Sieve
 tri = readdlm("067_triangle_tab.txt", Int32)
 
-function get_largest_path(row, col, total)
+# Matrix of zeros to store max values
+new_tri = zeros(99,99)
 
-    # Find the biggest path by comparing each one and finding the max in an array
-    o1 = tri[row , col] + tri[row + 1 , col]
-    o2 = tri[row , col] + tri[row + 1 , col + 1]
+@time for row in reverse(1:99)
+        for col in (1:length(tri[row,:][tri[row,:] .> 0]))
+                new_tri[row, col] = max(tri[row, col] + tri[row + 1, col],
+                tri[row, col] + tri[row + 1, col + 1])
+        end
 
-    o3 = tri[row , col + 1] + tri[row + 1 , col + 1]
-    o4 = tri[row , col + 1] + tri[row + 1 , col + 2]
-
-    # Increment the rows based on what the largest option was
-    max_array = [o1, o2, o3, o4]
-    mv, opt = findmax(max_array)
-
-
-    if opt[1 , 1] == 1
-        total += tri[row , col]
-        total += tri[row + 1 , col]
-        row += 1
-        col = col
-    elseif opt[1 , 1] == 2
-        total += tri[row , col]
-        total += tri[row + 1 , col + 1]
-        row += 1
-        col = col + 1
-    elseif opt[1 , 1] == 3
-        total += tri[row , col + 1]
-        total += tri[row + 1 , col + 1]
-        row += 1
-        col = col + 1
-    else
-        total += tri[row , col + 1]
-        total += tri[row + 1 , col + 2]
-        row += 1
-        col = col + 2
-    end
-
-    return row, col, total
+  # replace values in original Matrix with calculated max values
+  for col in (1:length(tri[row,:][tri[row,:] .> 0]))
+          tri[row, col] = new_tri[row, col]
+  end
 end
 
-total = 59
-row = 1
-col = 1
+tri[1:99, 1:99] #full pyramid of max values
 
-while row <= 100
-    row, col, total = get_largest_path(row, col, total)
-    row += 1
-end
-
-
-
-print("Problem 18 = ", total)  # 7273
+print("Problem 18 = ", tri[1, 1])
